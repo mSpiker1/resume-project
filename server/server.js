1fc9bb7fbb5ceac16ca1a802068d5c810d48286d
mSpiker1 <mspiker@embarqmail.com>
2025-06-25 19:19:52 -0400

    fix package.json for Railway deployment

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const os = require('os');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
