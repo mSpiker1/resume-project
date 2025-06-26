dcb53ef803acce23f9e7eed1524e4177e42de4a7
mSpiker1 <mspiker@embarqmail.com>
2025-06-25 22:54:20 -0400

    adjusted netlify.toml

--- .netlify/functions/server.js
+++ .netlify/functions/server.js
@@ -8,7 +8,7 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 });
 
-console.log(CLOUD_NAME, CLOUD_API_KEY);
+console.log("CLOUD_NAME, CLOUD_API_KEY");
 
 const app = express();
 const router = express.Router();
