2f7c285d223c0355a99ca909be9d85bc142e16a2
mSpiker1 <mspiker@embarqmail.com>
2025-06-25 22:09:00 -0400

    debug line to check production for an issue

--- client/src/App.js
+++ client/src/App.js
@@ -36,6 +36,8 @@ function App() {
     // Load the most up-to-date canvas art
     loadLatestCanvas();
 
+    fetch('/api/latest-canvas').then(res => res.text()).then(console.log);
+
     // Center the canvas
     const wrapper = canvasContainerRef.current;
     if (wrapper) {
