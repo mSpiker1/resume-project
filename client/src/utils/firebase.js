1463d877c55ac42b0a7e84224f6d0158416a7026
mSpiker1 <mspiker@embarqmail.com>
2025-07-03 19:23:56 -0400

    fix firebase environment variables (oops)

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: FIRE_API_KEY,
-    authDomain: "drawing-app-bf1a8.firebaseapp.com",
-    databaseURL: "https://drawing-app-bf1a8-default-rtdb.firebaseio.com",
-    projectId: "drawing-app-bf1a8",
-    storageBucket: "drawing-app-bf1a8.firebasestorage.app",
-    messagingSenderId: "752091274529",
-    appId: FIRE_APP_ID,
-    measurementId: "G-KWPYGHBLSV"
+    apiKey: process.env.FIRE_API_KEY,
+    authDomain: process.env.FIRE_AUTH_DOMAIN,
+    databaseURL: process.env.FIRE_DB_URL,
+    projectId: process.env.FIRE_PROJ_ID,
+    storageBucket: process.env.FIRE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRE_SENDER_ID,
+    appId: process.env.FIRE_APP_ID,
+    measurementId: process.env.FIRE_MEASURE_ID
 };
 
 const app = initializeApp(firebaseConfig);
